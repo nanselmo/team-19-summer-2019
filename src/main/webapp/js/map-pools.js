f570f69ee39d03f7aec6e5f0abc1e9cd8ac9f97d
Nicki Anselmo <nickianselmo@gmail.com>
2019-06-20 15:48:43 -0500

    initial commit

---
+++ src/main/webapp/js/map-pools.js
@@ -0,0 +1,141 @@
+var map;
+var infoWindow;
+var request;
+var request2;
+var request3;
+var request4;
+var service;
+var service2;
+var service3;
+var service4;
+var markers = [];
+
+/**
+ * Creates map view of default location with red markers
+ * on places that have outdoor pools. 
+ */
+function initialize() {
+    var center = new google.maps.LatLng(38.8819124,-77.1372792);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 13
+    });
+    request = {
+        location: center,
+        radius: 8047,
+        //types: ['pool'],
+        keyword: 'outdoor swimming pool'
+    };
+    infoWindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    request2 = {
+        location: center,
+        radius: 8047,
+        keyword: 'waterpark'
+    };
+    service2 = new google.maps.places.PlacesService(map);
+    service2.nearbySearch(request2, callback);
+
+    request3 = {
+        location: center,
+        radius: 8047,
+        keyword: 'outdoor pool'
+    };
+    service3 = new google.maps.places.PlacesService(map);
+    service3.nearbySearch(request3, callback);
+
+    request4 = {
+        location: center,
+        radius: 8047,
+        keyword: 'swim club'
+    }
+    service4 = new google.maps.places.PlacesService(map);
+    service4.nearbySearch(request4, callback);
+
+    /**
+     * When user right clicks on a different area on the map, the
+     * map shows outdoor pools in that area. 
+     */
+    google.maps.event.addListener(map, 'rightclick', function(event) {
+    map.setCenter(event.latLng);
+    clearResults(markers);
+
+    var request = {
+        location: event.latLng,
+        radius: 8047,
+        //types: ['pool'],
+        keyword: 'outdoor swimming pool'
+    };
+    service.nearbySearch(request, callback);
+
+    var request2 = {
+        location: event.latLng,
+        radius: 8047,
+        keyword: 'waterpark'
+    };
+    service2.nearbySearch(request2, callback);
+
+    var request3 = {
+        location: event.latLng,
+        radius: 8047,
+        keyword: 'outdoor pool'
+    };
+    service3.nearbySearch(request3, callback);
+
+    var request4 = {
+        location: event.latLng,
+        radius: 8047,
+        keyword: 'swim club'
+    }
+    service4.nearbySearch(request4, callback);
+    })
+}
+
+/**
+ * Create marker for each outdoor pool location and push marker to markers array.
+ * @param {var} results
+ * @param {var} status
+ */
+function callback(results, status) {
+    if(status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            console.log(results[i]);
+            markers.push(createMarker(results[i]));
+        }
+    }
+}
+
+/**
+ * Create marker and show info window with the place name when user clicks on marker.
+ * @param {Object} place
+ * @return {var} marker 
+ */
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(place.name);
+    infoWindow.open(map, this);
+    });
+    return marker;
+}
+
+/**
+ * Reset markers array to an empty array.
+ * @param {var} markers
+ */
+function clearResults(markers) {
+    for (var m in markers) {
+        markers[m].setMap(null);
+    }
+    markers = []
+}
+
+/* Display map when webpage loads. */
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ src/main/webapp/js/public-feed.js
@@ -0,0 +1,55 @@
+/** Fetches messages and add them to the page. */
+function fetchMessages(){
+  const url = '/feed';
+  fetch(url).then((response) => {
+    return response.json();
+  }).then((messages) => {
+    const messageContainer = document.getElementById('message-container');
+    if(messages.length == 0){
+      messageContainer.innerHTML = '<p>There are no posts yet.</p>';
+    }
+    else{
+      messageContainer.innerHTML = '';  
+    }
+    messages.forEach((message) => {  
+      const messageDiv = buildMessageDiv(message);
+      messageContainer.appendChild(messageDiv);
+    });
+  });
+}
+
+/**
+* Builds an element that displays the message.
+* @param {Message} message
+* @return {Element}
+*/
+function buildMessageDiv(message){
+  const usernameDiv = document.createElement('div');
+  usernameDiv.classList.add("left-align");
+  usernameDiv.appendChild(document.createTextNode(message.user));
+  
+  const timeDiv = document.createElement('div');
+  timeDiv.classList.add('right-align');
+  timeDiv.appendChild(document.createTextNode(new Date(message.timestamp)));
+  
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('message-header');
+  headerDiv.appendChild(usernameDiv);
+  headerDiv.appendChild(timeDiv);
+  
+  const bodyDiv = document.createElement('div');
+  bodyDiv.classList.add('message-body');
+  bodyDiv.appendChild(document.createTextNode(message.text));
+  
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add("message-div");
+  messageDiv.appendChild(headerDiv);
+  messageDiv.appendChild(bodyDiv);
+  
+  return messageDiv;
+}
+
+/** Fetches data and populates the UI of the page. */
+function buildUI(){
+  fetchMessages();
+}
